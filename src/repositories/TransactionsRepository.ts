65e6ffd660c044b6cf25e18fa18d8b65089097c7
Matheus Ferreira <matheusguiferreira@hotmail.com>
2020-04-27 23:51:51 -0300

    ADD challenge complete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: any;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accumulator, transaction) => {
+        accumulator[transaction.type] += transaction.value;
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    const { total } = this.getBalance();
+
+    if (transaction.type === 'outcome') {
+      if (total < transaction.value) {
+        throw Error(' your outcome is zero already');
+      }
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
